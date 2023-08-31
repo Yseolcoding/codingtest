28e42b1e3aa7fca990553ded250294e29d754800
YUNItoE <140488622+Yseolcoding@users.noreply.github.com>
2023-08-31 17:11:08 +0900

    Update 문자열 겹쳐쓰기.java
    delete syso

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -6,11 +6,8 @@ class Solution {
         String temp ="";
         
         strForward = my_string.substring(0,s);
-        System.out.println(strForward);
         temp = strForward+overwrite_string;
-        System.out.println(temp);
         strBack = my_string.substring(temp.length());
-        System.out.println(strBack);    
         answer = temp+strBack;
         
         return answer;
