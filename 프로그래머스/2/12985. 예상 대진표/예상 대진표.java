4957d625c4a43cccc0e7aed53c2a8921c10e6052
YUNItoE <140488622+Yseolcoding@users.noreply.github.com>
2023-10-11 18:09:22 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -8,17 +8,25 @@ class Solution
         int round = 0;
         int abRound = 1;
         
+        // 라운드 최대 진행횟수 구하기
         while(n>1){
             n = n/2;
             round++;
         }
         
+        // 라운드 진행
         for(int i=0; i<round; i++){
+            
+            // 같은조가 아닌 옆조로 배정됐을때, 종료를 막기 위한 조건에 필요.
             int min = Math.min(a,b);
+            
+            //같은조에 배정되었을 때, 반복문 종료 조건 (ex : 1,2 or 3,4)
             if(min%2!=0&&((a-b)==1||(b-a)==1)){
                 answer = abRound;
                 break;
             }
+            
+            //승리했을 때 다음라운드로 진출시 바뀌는 번호 배정
             if(a%2==1){
                 a = a/2 + 1;
             }else{
@@ -31,7 +39,6 @@ class Solution
                 b = b/2;
             }
             abRound++;
-            System.out.println("i : "+i+" a : " + a + " b : " + b + " abround : " + abRound);
         }
         answer = abRound;
         return answer;
