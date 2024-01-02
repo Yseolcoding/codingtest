af5dfba7961cb6b03db6aef20e6d8d7965a7ff50
YUNItoE <140488622+Yseolcoding@users.noreply.github.com>
2024-01-02 18:41:37 +0900

    [level unrated] Title: 편지, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/unrated/120898. 편지/편지.java
+++ 프로그래머스/unrated/120898. 편지/편지.java
@@ -1,11 +1,6 @@
 class Solution {
     public int solution(String message) {
-        int answer = 0;
-        char[] chMessage = message.toCharArray();
         
-        for(char a : chMessage){
-            answer = answer+2;
-        }
-        return answer;
+        return message.length()*2;
     }
 }
